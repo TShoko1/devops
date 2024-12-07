e337e3c6b0de105c7a2c0b3c38241ce14108082e
Shoko <113762194+TShoko1@users.noreply.github.com>
2024-12-07 23:45:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Обработчик для маршрута /livnes
-@app.route('/livnes', methods=['GET', 'POST'])
+@app.route('/liveness', methods=['GET', 'POST'])
 def livnes():
-    return jsonify({"message": "Livnes handler reached."}), 200
+    return jsonify({"message": "Liveness handler reached."}), 200
 
 # Обработчик для маршрута /rightness
-@app.route('/rightness', methods=['GET', 'POST'])
+@app.route('/readiness', methods=['GET', 'POST'])
 def rightness():
-    return jsonify({"message": "Rightness handler reached."}), 200
+    return jsonify({"message": "Readiness handler reached."}), 200
 
 if __name__ == '__main__':
     # Запуск приложения на localhost:5000
